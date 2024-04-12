7a5a942bf7471784785948c5470e18216b41cc89
R3tr0 <toby@soegaards.dk>
2024-04-12 10:08:54 +0200

    Initial commit med virkende kode + .gitignore

---
+++ bot.py
@@ -0,0 +1,21 @@
+import os
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+from dotenv import load_dotenv
+
+#Indlæser .env filen så den kan bruges
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix="<<", intents=discord.Intents.all())
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user}!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+#Kør botten
+bot.run(TOKEN)
