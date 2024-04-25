3a63c85345de4794e771ffa550795a1814cb1523
R3tr0 <toby@soegaards.dk>
2024-04-25 16:31:03 +0200

    Misc changes(factor of our code)

--- commands.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord import app_commands
-from discord.ext import commands
-
-
-class treeCMD(commands.Cog):
-    def __init__(self, bot: commands.bot):
-        self.bot = bot
