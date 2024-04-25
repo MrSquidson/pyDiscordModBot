740f9fe503dd4e79ee228a1ece6fe5695c5b83d9
R3tr0 <toby@soegaards.dk>
2024-04-25 16:30:19 +0200

    Start of Database
    Co-authored-by: R3tr0 <toby@soegaards.dk>
    Co-authored-by: Magnus H. <mfh1106@gmail.com>

--- Database.py
+++ Database.py
@@ -1,19 +1,29 @@
 import os
 import discord
 import csv
+from csv import DictWriter
 
 
 class Database:
+    filepath = os.path.join('./DB', discord.Guild.id)
+
     def __init__(self) -> None:
+        #Laver en filepath til brug senere
         pass
-            # Check if the folder already exists
-        if not os.path.exists(os.path.join('./DB', discord.Guild.id)):
-            #Laver en filepath til brug senere
-            filepath = os.path.join(os.path.join('./DB', discord.Guild.id))            
-            # Save cleaned content to file
-            with open(filepath, 'w', encoding='utf-8') as csv_file:
-                pass
     
-    def modAction(self, guild, modID, offenderID, action):
-        if os.path.exists(os.path.join('./DB', guild, 'punishments.csv')):
-            csv.writer()
+    def modAction(self, guildID:int, modID:int, offenderID:int, timeStart:int, timeEnd:int, action:str):
+        field_names = ['modID', 'offenderID', 'timeStart', 'timeEnd', 'action']
+
+        if not os.path.exists(os.path.join('./DB', guildID, 'punishments.csv')): # Hvis './DB/GuildID/punishments.csv' ikke findes
+            with open('punishments.csv','w', newline='') as csvfile: #Opret ny csv fil
+                writer = csv.DictWriter(csvfile, fieldnames=field_names) # Med field_names i headeren (indsætter også header)
+                writer.writeheader()
+                writer.writerow({f'modId': modID, 'offenderID': offenderID,'timeStart': timeStart,'timeEnd': timeEnd, 'action': action}) # Skriv case 1
+                print('Made new .csv file for GuildID')
+
+        else: # ... 'punishments.csv' findes!
+            with open(os.path.join(filepath, 'punishments.csv'),'a', newline='') as f_object: # åben filen i append mode!
+                dictwriter_object = DictWriter(f_object, fieldnames=field_names) # med de samme fieldnames som før
+                dictwriter_object.writerow({modID, offenderID, timeStart, timeEnd, action}) # Skriv den nye case ind i filen
+                print('Appended most recent case')
+
