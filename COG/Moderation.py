2eccc07c05b53cf90c135a964f42792523d9d56c
R3tr0 <toby@soegaards.dk>
2024-05-01 13:14:50 +0200

    Embed userinfo work
    Co-authored-by: Magnus H. <MrSquidson@users.noreply.github.com>
    Co-authored-by: MantaRay3 <MantaRay3@users.noreply.github.com>

--- COG/Moderation.py
+++ COG/Moderation.py
@@ -38,11 +38,16 @@ class treeCMD(commands.Cog):
     
 
     # Public User Info      
-    @app_commands.command(name='user_info', description='Get info of any user in this server')
-    @app_commands.describe(user='select a user')
+    @app_commands.command(
+        name='user_info', 
+        description='Get info of any user in this server'
+    )
+    @app_commands.describe(
+        user='select a user'
+    )
 
-    async def user_info(self, interaction: discord.Interaction, user: Member):
-        self.embed = discord.Embed(title=f"User Info about {user}", color=user.color, timestamp=interaction.message.created_at)
+    async def userinfo(self, interaction: discord.Interaction, user: Member):
+        self.embed = discord.Embed(title=f"User Info about {user}")
         self.embed.author(name='Username', value=user.name)
         self.embed.thumbnail(url=user.avatar.url)
         self.embed.add_field(name='Discord @', value=user.discriminator)
@@ -50,29 +55,20 @@ class treeCMD(commands.Cog):
         self.embed.add_field(name='Current status', value=user.status)
         self.embed.add_field(name='Server Join', value=user.joined_at)
         self.embed.add_field(name='Account age', value=user.created_at)
+        await interaction.response.send_message(embed=self.embed)
 
-        self.embed.footer()
-
-        pass
 
     # Ban command and stuff
     # @app_commands.command(name='ban', description='Ban a user')
     # @app_commands.describe(name='reason', description='give a reason for ban')
     # @has_permissions(ban_members = True)
-    # async def ban(self, interaction: discord.Interaction, user: Member, *, reason: str):
+    # async def ban(self, interaction: discord.Interaction, user: Member, reason: str):
     #     try:
     #         await interaction.user.ban(reason=reason)
     #         await interaction.response.send_message(f'User {user} has been banned')
     #     except Exception as e:
     #         print(e)
-    #         await interaction.response.send_message('Cannot ban user')
-    # # If a user who is not allowed to ban try's the ban command    
-    # @ban.error
-    # async def ban_error(interaction, error): 
-    #     #try:??
-    #         if isinstance(error, commands.MissingPermissions):
-    #             await interaction.send ("You do not have permession to ban")
-            
+    #         await interaction.response.send_message('Cannot ban user')            
     # pass
 
         
