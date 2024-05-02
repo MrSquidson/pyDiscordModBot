1086e3f95e12f7c5e9831513ccb0f6cdee74e517
R3tr0 <toby@soegaards.dk>
2024-05-02 13:10:56 +0200

    Embed changes Fixed Ban command Added Unban and kick command
    Co-authored-by: Magnus H. <MrSquidson@users.noreply.github.com>
    Co-authored-by: MantaRay3 <MantaRay3@users.noreply.github.com>

--- COG/Moderation.py
+++ COG/Moderation.py
@@ -7,6 +7,8 @@ import typing
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.presences = True
+
 class Moderation(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -33,19 +35,19 @@ class Moderation(commands.Cog):
     )
 
     async def userinfo(self, interaction: discord.Interaction, user: Member):
-        self.embed = discord.Embed(title=f"User Info about {user}")
-        self.embed.author(name='Username', value=user.name)
-        self.embed.thumbnail(url=user.avatar.url)
-        self.embed.add_field(name='Discord @', value=user.discriminator)
-        self.embed.add_field(name='UserID', value=user.id)
-        self.embed.add_field(name='Current status', value=user.status)
-        self.embed.add_field(name='Server Join', value=user.joined_at)
-        self.embed.add_field(name='Account age', value=user.created_at)
-        await interaction.followup.send(embed=self.embed)
+        embed = discord.Embed(title=f"User Info about {user}")
+        embed.set_thumbnail(url=user.avatar.url)
+        embed.set_author(name=f'Authored by {interaction.user}', icon_url=interaction.user.avatar.url)
+        embed.set_footer(text=f'Discord @ {user}')
+        embed.add_field(name='UserID', value=user.id, inline=True)
+        embed.add_field(name='Current status', value=user.raw_status, inline=True)
+        embed.add_field(name='Server Join', value=user.joined_at.strftime("%B %d %Y"), inline=True)
+        embed.add_field(name='Account age', value=user.created_at.strftime("%B %d %Y"), inline=True)
+        await interaction.response.send_message(embed=embed)
     
     # Purge Messages indiscriminately in a channel
     @app_commands.command(
-        name="purgemsg_1",
+        name="purgemsg",
         description="A command that deletes a specified message",
     )
     @app_commands.describe(
@@ -55,25 +57,53 @@ class Moderation(commands.Cog):
         reason='The Reason'
     )
 
-    async def purge_command(self, interaction: discord.Interaction, amount: int, reason: str):
+    async def purge_command(self, interaction: discord.Interaction, amount: int, reason: typing.Optional[str]):
         self.channel = interaction.channel
         await interaction.response.send_message('Attempting to purge messages!', ephemeral=True)
-        dltMsgAmount = await self.channel.purge(limit=amount, reason=typing.Optional[app_commands.Choice[str]])
+        dltMsgAmount = await self.channel.purge(limit=amount, reason=reason)
         await interaction.followup.send(f'Deleted {len(dltMsgAmount)} Messages!', ephemeral=True)
     
 
     # Ban command and stuff
-    # @app_commands.command(name='ban', description='Ban a user')
-    # @app_commands.describe(name='reason', description='give a reason for ban')
-    # @has_permissions(ban_members = True)
-    # async def ban(self, interaction: discord.Interaction, user: Member, reason: str):
-    #     try:
-    #         await interaction.user.ban(reason=reason)
-    #         await interaction.response.send_message(f'User {user} has been banned')
-    #     except Exception as e:
-    #         print(e)
-    #         await interaction.response.send_message('Cannot ban user')            
-    # pass
+    @app_commands.command(name='ban', description='ban a user')
+    @app_commands.describe(user='User you are banning')
+    @app_commands.describe(reason='Reason the user is getting banned')
+    @app_commands.describe(dltmsg='Deletes the users messages x days back')
+    @has_permissions(ban_members = True)
+    async def ban(self, interaction: discord.Interaction, user: Member, reason: typing.Optional[str], dltmsg: typing.Optional[int]):
+        try:
+            await user.ban(reason=reason,delete_message_days=dltmsg)
+            await interaction.response.send_message(f'User {user} has been banned with the reason: \n "{reason}"')
+        except Exception as e:
+            print(e)
+            await interaction.response.send_message(f'Cannot ban user, {e}') 
+
+    # unban command and stuff
+    @app_commands.command(name='unban', description='unban a user')
+    @app_commands.describe(user='User you are unbanning')
+    @app_commands.describe(reason='Reason the user is getting unbanned')
+    @has_permissions(administrator = True)
+    async def unban(self, interaction: discord.Interaction, user: Member, reason: typing.Optional[str]):
+        try:
+            await user.ban(reason=reason)
+            await interaction.response.send_message(f'User {user} has been unbanned with the reason: \n "{reason}"')
+        except Exception as e:
+            print(e)
+            await interaction.response.send_message(f'Cannot unban user, {e}') 
+
+    # kick command and stuff
+    @app_commands.command(name='kick', description='kick a user')
+    @app_commands.describe(user='User you are kicking')
+    @app_commands.describe(reason='Reason the user is getting kick')
+    @has_permissions(kick_members = True)
+    async def  kick(self, interaction: discord.Interaction, user: Member, reason: typing.Optional[str]):
+        try:
+            await user.kick(reason=reason)
+            await interaction.response.send_message(f'User {user} has been kicked with the reason: \n "{reason}"')
+        except Exception as e:
+            print(e)
+            await interaction.response.send_message(f'Cannot kick user, {e}') 
+          
 
         
         
